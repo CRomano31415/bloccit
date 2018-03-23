5a2dc0e97e6d66427837b4058e8bb56cd1eb58e1
Claudia <cromano31415@gmail.com>
2018-03-22 21:24:49 -0500

    added SPAM change on the controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,12 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all 
+    @posts = Post.all
+    #add spam for every 5th
+    @posts.each_with_index do |post, index|
+      if index%5 == 0 
+        post.title = "SPAM"
+      end
+    end
   end
 
   def show
