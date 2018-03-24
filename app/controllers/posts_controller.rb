00bb8c953304ceb87231fb2e3e1e32cccd44f014
Claudia <cromano31415@gmail.com>
2018-03-23 21:13:53 -0500

    added create and read functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,17 +3,34 @@ class PostsController < ApplicationController
     @posts = Post.all
     #add spam for every 5th
     @posts.each_with_index do |post, index|
-      if index%5 == 0 
+      if index%5 == 0
         post.title = "SPAM"
       end
     end
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
+    @post = Post.new
   end
+
+  def create
+    @post = Post.new
+    @post.title = params[:post][:title]
+    @post.body = params[:post][:body]
+
+    if @post.save
+      flash[:notice] = "Post was saved."
+      redirect_to @post
+    else
+      flash.now[:alert] = "There was an error saving the post. Please try again."
+      render :new
+    end
+  end
+
 
   def edit
   end
