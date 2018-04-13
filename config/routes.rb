9233cd559a8e7b53c1647dc551d748220c092edb
Claudia <cromano31415@gmail.com>
2018-04-12 21:28:32 -0500

    Comment partial and associations

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ resources :topics do
   resources  :sponsored_posts, except: [:index]
 end
 
+resources :posts, only: [] do
+	resources :comments, only: [:create, :destroy]	
+end
+
 resources :users, only: [:new, :create]
 resources :sessions, only: [:new, :create, :destroy]
 post 'users/confirm' => 'users#confirm'
--- db/seeds.rb
+++ db/seeds.rb
@@ -61,6 +61,7 @@ questions = Question.all
 
 100.times do
     Comment.create!(
+      user: users.sample,
       post: posts.sample,
       body: RandomData.random_paragraph
     )
