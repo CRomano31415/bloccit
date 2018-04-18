dff7c44551366bc1e14aaaaf49faf22c6fd09c1c
Claudia <cromano31415@gmail.com>
2018-04-17 20:20:11 -0500

    added factory gem and refractored code

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,4 +33,9 @@ before_save { self.role ||= :member }
     favorites.where(post_id: post.id).first
   end 
 
+   def avatar_url(size)
+     gravatar_id = Digest::MD5::hexdigest(self.email).downcase
+     "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
+   end
+
 end
